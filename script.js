aec39e3a27f5b912a53d1be314c4efe4550f4f7a
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-08 10:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ function Car(make, model) {
 	this.model=model;
 }
 
-Car.prototype.getMakeModel=function(){
+Car.prototype.getMakeModel =function(){
 		return `${this.make} ${this.model}`;
 	};
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,model,topSpeed);
+	Car.call(this,make,model)
 	this.topSpeed=topSpeed;
 }
-
-Sportscar.prototype=Object.create(Car,prototype);
+	
+SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 
 SportsCar.prototype.getTopSpeed=function(){
 		return this.topSpeed;
-	};
+	}
 
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-car.getTopSpeed();
-car.getMakeModel();
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// console.log(car.make);
+// console.log(car.model);
+// console.log(car.topSpeed);
+// console.log(car.getMakeModel());
+// console.log(car.getTopSpeed());
+
 
 // Do not change the code below
 window.Car = Car;
