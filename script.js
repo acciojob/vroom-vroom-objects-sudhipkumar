5a04c8900329165445843a5a061777d199d10ae9
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-08 10:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 function Car(make, model) {
 	this.make=make;
 	this.model=model;
+}
 
-	Car.prototype.getMakeModel=function(){
+Car.prototype.getMakeModel=function(){
 		return `${this.make} ${this.model}`;
 	}
-}
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,model,topSpeed);
 	this.topSpeed=topSpeed;
-
-	SportsCar.prototype.getTopSpeed=function(){
-		return this.topSpeed;
-	}
 }
 
 Sportscar.prototype=Object.create(Car,prototype);
 SportsCar.prototype.constructor=SportsCar;
+
+SportsCar.prototype.getTopSpeed=function(){
+		return this.topSpeed;
+	}
 
 // Do not change the code below
 window.Car = Car;
