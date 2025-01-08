2a10ec88fc517a179fe15750c4f8c6b8caac237c
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-08 09:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
 
-function SportsCar(make, model, topSpeed) {}
+	getMakeModel(){
+		return `${this.make} ${this.model}`;
+	}
+}
+
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed;
+
+	getTopSpeed(){
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
